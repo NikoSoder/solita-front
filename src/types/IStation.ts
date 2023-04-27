ded15748ab44e68b9a96825f211916b64764a5d7
Niko <soder.n@gmail.com>
2023-04-27 13:19:49 +0300

    Create api response types

---
+++ src/types/IStation.ts
@@ -0,0 +1,14 @@
+export interface Station {
+  FID: number;
+  ID: number;
+  Nimi: string;
+  Namn: string;
+  Name: string;
+  Osoite: string;
+  Stad: string;
+  Operaattor: string;
+  Kapasiteet: number;
+  x: number;
+  y: number;
+  id: string;
+}
