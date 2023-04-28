4b3fc386afb1499a4cc4c8ee27381b1ddc72c8ce
Niko <soder.n@gmail.com>
2023-04-28 21:43:50 +0300

    Create and style journeys table

---
+++ src/components/Table.tsx
@@ -0,0 +1,36 @@
+import { Trip } from "../types/ITrip";
+type ChildPropsTable = {
+  trips: Trip[];
+};
+// TODO: change covered distance to km and duration to minutes **************
+const Table = ({ trips }: ChildPropsTable) => {
+  return (
+    <div className="overflow-x-auto">
+      <table className="table-auto bg-white shadow-2xl dark:bg-slate-800">
+        <thead>
+          <tr className="bg-slate-200 dark:bg-slate-700 dark:text-slate-300">
+            <th>Departure</th>
+            <th>Return</th>
+            <th>Distance (km)</th>
+            <th>Duration (min)</th>
+          </tr>
+        </thead>
+        <tbody>
+          {trips.map((trip) => (
+            <tr
+              key={trip.id}
+              className="border-b border-slate-200 text-slate-700 dark:border-slate-700 dark:text-slate-400"
+            >
+              <td>{trip.departureStationName}</td>
+              <td>{trip.returnStationName}</td>
+              <td>{trip.coveredDistance}</td>
+              <td>{trip.duration}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Table;
