627317949013b493eaff86ea5ee445dfcf434b4c
Niko <soder.n@gmail.com>
2023-05-01 14:34:27 +0300

    Show loading if fetching data

--- src/App.tsx
+++ src/App.tsx
@@ -33,11 +33,19 @@ const App = () => {
     fetchData();
   }, []);
 
+  if (!trips.length || !stations.length) {
+    return (
+      <div className="flex h-screen flex-col items-center gap-3 bg-slate-50 dark:bg-gray-900">
+        <p className="text-xl dark:text-blue-100">Loading please wait...</p>
+        <div className="h-10 w-10 animate-spin rounded-full border-2 border-t-blue-500"></div>
+      </div>
+    );
+  }
   return (
-    <div className="bg-slate-50 dark:bg-gray-900">
+    <div className="h-full bg-slate-50 dark:bg-gray-900">
       <Router>
         <Navbar />
-        <div className="container mx-auto p-3">
+        <div className="container mx-auto p-3 py-10">
           <Routes>
             <Route
               path="/"
