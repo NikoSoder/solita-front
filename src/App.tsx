79bda165079b3b72a99372c993558fdeb433c437
Niko <soder.n@gmail.com>
2023-05-01 16:06:54 +0300

    Clean up App component

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import apiService from "./services/api-service";
 import { useEffect, useState } from "react";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
+import { Loading } from "./components/Loading";
 
 const App = () => {
   const [trips, setTrips] = useState<Trip[]>([]);
@@ -33,32 +34,28 @@ const App = () => {
     fetchData();
   }, []);
 
-  if (!trips.length || !stations.length) {
-    return (
-      <div className="flex h-screen flex-col items-center gap-3 bg-slate-50 dark:bg-gray-900">
-        <p className="text-xl dark:text-blue-100">Loading please wait...</p>
-        <div className="h-10 w-10 animate-spin rounded-full border-2 border-t-blue-500"></div>
-      </div>
-    );
-  }
   return (
     <div className="h-full bg-slate-50 dark:bg-gray-900">
       <Router>
         <Navbar />
         <div className="container mx-auto p-3 py-10">
-          <Routes>
-            <Route
-              path="/"
-              element={
-                <Home
-                  trips={trips}
-                  setTrips={setTrips}
-                  stations={stations}
-                  setStations={setStations}
-                />
-              }
-            />
-          </Routes>
+          {!trips.length || !stations.length ? (
+            <Loading />
+          ) : (
+            <Routes>
+              <Route
+                path="/"
+                element={
+                  <Home
+                    trips={trips}
+                    setTrips={setTrips}
+                    stations={stations}
+                    setStations={setStations}
+                  />
+                }
+              />
+            </Routes>
+          )}
         </div>
       </Router>
     </div>
