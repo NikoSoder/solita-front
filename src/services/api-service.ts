b36cdf0a896f5abd60b6cad08f1db749617e8c42
Niko <soder.n@gmail.com>
2023-05-05 16:10:38 +0300

    Update service and trip interface

--- src/services/api-service.ts
+++ src/services/api-service.ts
@@ -1,26 +1,32 @@
 import axios from "axios";
-import { Trip } from "../types/ITrip";
-import { Station } from "../types/IStation";
+import { ITrip } from "../types/ITrip";
+import { IStation } from "../types/IStation";
+import { IPageResponse } from "../types/ITrip";
 const baseUrl = "http://localhost:3000/api/";
 
-const getTrips = async (): Promise<Trip[]> => {
+const getTrips = async (): Promise<IPageResponse> => {
   const response = await axios.get(baseUrl + "trips");
   return response.data;
 };
 
-const getStations = async (): Promise<Station[]> => {
+const getStations = async (): Promise<IStation[]> => {
   const response = await axios.get(baseUrl + "stations");
   return response.data;
 };
 
-const getTrip = async (id: string): Promise<Station> => {
+const getTrip = async (id: string): Promise<IStation> => {
   const response = await axios.get(`${baseUrl}trips/${id}`);
   return response.data;
 };
 
-const getStation = async (id: string): Promise<Station> => {
+const getStation = async (id: string): Promise<IStation> => {
   const response = await axios.get(`${baseUrl}stations/${id}`);
   return response.data;
 };
 
-export default { getTrips, getStations, getTrip, getStation };
+const getPage = async (page: number): Promise<IPageResponse> => {
+  const response = await axios.get(`${baseUrl}trips/${page}`);
+  return response.data;
+};
+
+export default { getTrips, getStations, getTrip, getStation, getPage };
