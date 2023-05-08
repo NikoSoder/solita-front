49acf098de36d5cde49fe0934091567e6174aee5
Niko <soder.n@gmail.com>
2023-05-08 18:27:47 +0300

    Style logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,19 +1,22 @@
-import { Link } from "react-router-dom";
 import { SunIcon } from "@heroicons/react/24/solid";
 import { MoonIcon } from "@heroicons/react/24/solid";
 import { useState } from "react";
+import { GlobeAltIcon } from "@heroicons/react/24/solid";
 
 const Navbar = () => {
   const [theme, setTheme] = useState(true);
-
+  //
   const toggleTheme = () => {
     document.documentElement.classList.toggle("dark");
     setTheme(!theme);
   };
   return (
-    <nav className="flex items-center justify-around bg-center p-2 dark:text-gray-300">
+    <nav className="relative flex items-center justify-between bg-center px-2 py-2 dark:text-gray-300 sm:px-10">
+      <GlobeAltIcon className="absolute left-3 top-0 h-8 w-8 text-sky-700" />
       <div>
-        <Link to="/">Home</Link>
+        <h1 className="logo-font ms-8 bg-gradient-to-r from-sky-700 to-teal-800 bg-clip-text text-xl tracking-wider text-transparent dark:to-teal-600 sm:ms-0 sm:text-2xl">
+          City Bike Helsinki
+        </h1>
       </div>
       <div>
         {theme ? (
