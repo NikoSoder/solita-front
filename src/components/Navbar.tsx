a64f8f845e1025587f92772c72df7aa83dc4775f
Niko <soder.n@gmail.com>
2023-05-08 14:46:02 +0300

    Add theme toggle button

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,32 @@
 import { Link } from "react-router-dom";
+import { SunIcon } from "@heroicons/react/24/solid";
+import { MoonIcon } from "@heroicons/react/24/solid";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [theme, setTheme] = useState(true);
+
+  const toggleTheme = () => {
+    document.documentElement.classList.toggle("dark");
+    setTheme(!theme);
+  };
   return (
-    <nav className="bg-white p-5 shadow dark:bg-slate-800 dark:text-gray-300">
+    <nav className="flex items-center justify-around bg-white p-5 shadow dark:bg-slate-800 dark:text-gray-300">
       <div>
         <Link to="/">Home</Link>
+      </div>
+      <div>
+        {theme ? (
+          <SunIcon
+            onClick={toggleTheme}
+            className="main-animation h-6 w-6 cursor-pointer"
+          />
+        ) : (
+          <MoonIcon
+            onClick={toggleTheme}
+            className="main-animation h-6 w-6 cursor-pointer"
+          />
+        )}
       </div>
     </nav>
   );
