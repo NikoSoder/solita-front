e17974c7625bf7423b311d0aa9994f92605b918c
Niko <soder.n@gmail.com>
2023-06-05 13:13:25 +0300

    Remove a tag in Station component

--- src/components/Station.tsx
+++ src/components/Station.tsx
@@ -9,44 +9,39 @@ type ChildPropsStation = {
 
 const Station = ({ selected, stationStats }: ChildPropsStation) => {
   return (
-    <a
-      href={`https://www.google.com/maps/search/${selected.name}`}
-      target="_blank"
-    >
-      <div className="max-w-sm rounded border border-transparent bg-white p-6 text-slate-600 shadow-md hover:border-sky-600 dark:border dark:border-slate-500 dark:bg-slate-800 dark:text-slate-300 dark:hover:border-sky-600">
-        <div className="mb-6 border-b dark:border-slate-700">
-          <h2 className="text-lg text-slate-900 dark:text-slate-200">
-            {selected.name}
-          </h2>
+    <div className="max-w-sm rounded border border-transparent bg-white p-6 text-slate-600 shadow-md dark:border dark:border-slate-500 dark:bg-slate-800 dark:text-slate-300">
+      <div className="mb-6 border-b dark:border-slate-700">
+        <h2 className="text-lg text-slate-900 dark:text-slate-200">
+          {selected.name}
+        </h2>
+      </div>
+      <div className="-ml-1 flex flex-col gap-5">
+        <div className="flex items-center gap-2">
+          <div className="rounded-full bg-slate-200 p-1 dark:bg-slate-400">
+            <MapPinIcon className="h-4 w-4 text-slate-700 dark:text-slate-900" />
+          </div>
+          <p> {selected.osoite} </p>
         </div>
-        <div className="-ml-1 flex flex-col gap-5">
-          <div className="flex items-center gap-2">
-            <div className="rounded-full bg-slate-200 p-1 dark:bg-slate-400">
-              <MapPinIcon className="h-4 w-4 text-slate-700 dark:text-slate-900" />
-            </div>
-            <p> {selected.osoite} </p>
+        <div className="flex items-center gap-2">
+          <div className="rounded-full bg-green-200 p-1 dark:bg-green-400">
+            <CheckIcon className="h-4 w-4 text-green-900" />
           </div>
-          <div className="flex items-center gap-2">
-            <div className="rounded-full bg-green-200 p-1 dark:bg-green-400">
-              <CheckIcon className="h-4 w-4 text-green-900" />
-            </div>
-            <p>Journeys started</p>
-            <p className="text-2xl font-bold text-slate-900 dark:text-slate-200">
-              {stationStats.departureCount}
-            </p>
+          <p>Journeys started</p>
+          <p className="text-2xl font-bold text-slate-900 dark:text-slate-200">
+            {stationStats.departureCount}
+          </p>
+        </div>
+        <div className="flex items-center gap-2">
+          <div className="rounded-full bg-green-200 p-1 dark:bg-green-400">
+            <CheckIcon className="h-4 w-4 text-green-900" />
           </div>
-          <div className="flex items-center gap-2">
-            <div className="rounded-full bg-green-200 p-1 dark:bg-green-400">
-              <CheckIcon className="h-4 w-4 text-green-900" />
-            </div>
-            <p>Journeys ended</p>
-            <p className="text-2xl font-bold text-slate-900 dark:text-slate-200">
-              {stationStats.returnCount}
-            </p>
-          </div>
+          <p>Journeys ended</p>
+          <p className="text-2xl font-bold text-slate-900 dark:text-slate-200">
+            {stationStats.returnCount}
+          </p>
         </div>
       </div>
-    </a>
+    </div>
   );
 };
 
