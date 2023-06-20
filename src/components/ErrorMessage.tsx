8f6bc80a93301d5af0996620b39b6108b407ad74
Niko <soder.n@gmail.com>
2023-06-20 15:11:22 +0300

    Create fallback error message

---
+++ src/components/ErrorMessage.tsx
@@ -0,0 +1,9 @@
+const ErrorMessage = () => {
+  return (
+    <div className="bg-red-500 p-2 text-center font-semibold text-white">
+      <h2>Something went wrong. Try again later.</h2>
+    </div>
+  );
+};
+
+export default ErrorMessage;
