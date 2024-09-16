f526ad2fdcd0505eab353427c13c5bd79690d329
Niko <soder.n@gmail.com>
2024-09-16 19:02:20 +0300

    feat: add weekday tabs to choose active day

--- src/components/PeakTimes.tsx
+++ src/components/PeakTimes.tsx
@@ -1,7 +1,9 @@
+// TODO:
+// fix height change when switching days
 import { keepPreviousData, useQuery } from "@tanstack/react-query";
 import apiService from "../services/api-service";
 import { useState } from "react";
-import { LoadingSkeleton } from "./Loading";
+import { LoadingSkeleton, LoadingSmall } from "./Loading";
 
 type ChildPropsPeakTimes = {
   activeStationId: string;
@@ -19,7 +21,7 @@ enum DayOfTheWeek {
 
 export function PeakTimes({ activeStationId }: ChildPropsPeakTimes) {
   const [weekDay, setWeekDay] = useState<DayOfTheWeek>(DayOfTheWeek.Monday);
-  const { isPending, isError, error, data, isFetching } = useQuery({
+  const { isPending, error, data, isFetching } = useQuery({
     queryKey: ["peakTimes", { activeStationId, weekDay }],
     queryFn: () => apiService.getPeakTimes(activeStationId, weekDay),
     placeholderData: keepPreviousData,
@@ -33,15 +35,45 @@ export function PeakTimes({ activeStationId }: ChildPropsPeakTimes) {
   if (error) {
     return <div>Error: {error.message}</div>;
   }
+  const dayNames = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
 
   return (
     <div className="overflow-x-auto rounded bg-white p-6 text-slate-600 shadow-md dark:border dark:border-slate-500 dark:bg-slate-800 dark:text-slate-300">
+      <div className="mb-10 flex justify-between gap-10">
+        {dayNames.map((dayName) => (
+          <button
+            key={dayName}
+            onClick={() =>
+              setWeekDay(DayOfTheWeek[dayName as keyof typeof DayOfTheWeek])
+            }
+            className={`${
+              weekDay === DayOfTheWeek[dayName as keyof typeof DayOfTheWeek]
+                ? "underline underline-offset-8"
+                : ""
+            }`}
+          >
+            {dayName.slice(0, 3)}
+          </button>
+        ))}
+      </div>
+      {/* {isFetching ? <LoadingSmall /> : null} */}
       <div className="flex items-end justify-between gap-10">
         {data.map((hour) => (
-          <div className="flex flex-col items-center gap-2">
+          <div
+            key={hour.hour_of_day}
+            className="flex flex-col items-center gap-2"
+          >
             <p
               style={{ height: `${hour.departures}px` }}
-              className="w-full rounded-md bg-slate-500"
+              className="w-3 rounded-md bg-slate-500"
             ></p>
             <p>{hour.departures}</p>
             <p>{hour.hour_of_day}</p>
