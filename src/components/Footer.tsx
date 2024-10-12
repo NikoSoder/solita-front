ddd03a269eeba780928d673f49b48f33b89392a3
NikoSoder <105541058+NikoSoder@users.noreply.github.com>
2024-10-12 11:09:31 +0300

    fix(2024 rewrite): typos and icons (#47)
    * fix: typos
    
    * style: add icons to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,14 +1,25 @@
+import {
+  ArrowTopRightOnSquareIcon,
+  EnvelopeIcon,
+} from "@heroicons/react/24/outline";
+
 const Footer = () => {
   return (
     <footer className="flex flex-col items-center gap-5 px-5 py-3 text-sm text-slate-500 dark:text-slate-400">
       <p className="italic text-slate-600 dark:text-slate-300">
-        Designed and build by Niko Söder.
+        Designed and built by Niko Söder.
       </p>
       <div className="flex flex-col gap-4">
-        <a href="https://github.com/NikoSoder/solita-front" target="_blank">
-          Github
-        </a>
-        <a href="mailto:soder.n@gmail.com">Email</a>
+        <div className="flex items-center gap-1">
+          <ArrowTopRightOnSquareIcon className="h-4 w-4" />
+          <a href="https://github.com/NikoSoder/solita-front" target="_blank">
+            Github
+          </a>
+        </div>
+        <div className="flex items-center gap-1">
+          <EnvelopeIcon className="h-4 w-4" />
+          <a href="mailto:soder.n@gmail.com">Email</a>
+        </div>
       </div>
     </footer>
   );
