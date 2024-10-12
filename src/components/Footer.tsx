68319292393ac18419e773f3daf22e0219fc58a9
Niko <soder.n@gmail.com>
2024-10-12 11:06:39 +0300

    style: add icons to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,8 @@
+import {
+  ArrowTopRightOnSquareIcon,
+  EnvelopeIcon,
+} from "@heroicons/react/24/outline";
+
 const Footer = () => {
   return (
     <footer className="flex flex-col items-center gap-5 px-5 py-3 text-sm text-slate-500 dark:text-slate-400">
@@ -5,10 +10,16 @@ const Footer = () => {
         Designed and built by Niko Söder.
       </p>
       <div className="flex flex-col gap-4">
-        <a href="https://github.com/NikoSoder/solita-front" target="_blank">
-          Github
-        </a>
-        <a href="mailto:soder.n@gmail.com">Email</a>
+        <div className="flex items-center gap-1">
+          <ArrowTopRightOnSquareIcon className="h-4 w-4" />
+          <a href="https://github.com/NikoSoder/solita-front" target="_blank">
+            Github
+          </a>
+        </div>
+        <div className="flex items-center gap-1">
+          <EnvelopeIcon className="h-4 w-4" />
+          <a href="mailto:soder.n@gmail.com">Email</a>
+        </div>
       </div>
     </footer>
   );
